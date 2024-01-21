074506c440e166f22ebbfbd3467a7439aef61733
Jiabo Zheng <804552226@qq.com>
2024-01-21 23:14:03 +0800

    chore: tsc vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
-
 export default mergeConfig(
-  viteConfig,
+  viteConfig({ command: 'build', mode: 'production' }),
   defineConfig({
     test: {
       environment: 'jsdom',
